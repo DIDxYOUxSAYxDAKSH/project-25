13510bc801ec37a772fa55c8ef412be77aab1d70
DIDxYOUxSAYxDAKSH <75373199+DIDxYOUxSAYxDAKSH@users.noreply.github.com>
2021-02-03 19:01:24 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class PAPER{
+constructor(x,y,radius){
+ var op={
+    isStatic: false,
+    'restitution':0.3,
+    'friction':0.5,
+    'density':1.2,
+}
+ this.body = Matter.Bodies.circle(x,y,radius,op);
+ this.radius = radius;
+ this.img1 = loadImage("paper.png");
+ World.add(world,this.body); 
+ 
+
+}
+display(){
+ var posX = this.body.position.x;
+ var posY = this.body.position.y;
+ push();
+ imageMode(RADIUS);
+ fill(255,255,255);
+ image(this.img1,posX,posY,this.radius,this.radius);
+ pop();
+}
+
+
+
+
+}
